9be0cf2266f368712d03a4cd505963386b989ced
Maksym <citomax7196@gmail.com>
2024-01-22 12:56:54 +0100

    add logic for start page

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   const levels = ['Easy', 'Medium', 'Hard']
   const choicesList = document.querySelector('#choices')
-  const choiceLevelBtn = document.querySelector('#choiceLevelBtn')
+  const choiceLevelBtn = document.querySelector('#choice-level-btn')
+  const startCanvas = document.querySelector('#start-canvas')
+  const gameCanvas = document.querySelector('#game-canvas')
+  const resultCanvas = document.querySelector('#result-canvas')
 
-
+  const level = new DifficultyLevel()
 
   levels.forEach((level) => {
     const liElement = document.createElement('li')
@@ -15,7 +18,16 @@ document.addEventListener('DOMContentLoaded', () => {
   })
 
   const levelBtnHandler = () => {
-    console.log(213);
+    const choices = document.querySelectorAll('input[name=choice]');
+    const choice = [...choices].filter(i => i.checked);
+    if (choice.length) {
+      level.changeLevel(choice[0].value)
+      // console.log(levelScore.checkLevelHandler());
+      startCanvas.style.display = 'none';
+      gameCanvas.style.display = 'flex';
+
+    }
+
   }
 
 
